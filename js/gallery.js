2be89141c832fb4fd7d2720a6ad77a612412c3e6
Samuel Pelletier <pellesam.info@gmail.com>
2025-07-09 23:15:28 +0200

    Update image link

--- js/gallery.js
+++ js/gallery.js
@@ -3,36 +3,116 @@ function urlParam(name) {
     return results == null ? null : results[1];
 }
 
+var IMAGES_PER_PAGE = 12;
+var galleryData = [];
+var currentPage = 1;
+var totalPages = 1;
+
+var areaToPrefectures = {
+    1: [1],
+    2: [2, 3, 4, 5, 6, 7],
+    3: [8, 9, 10, 11, 12, 13, 14],
+    4: [15, 16, 17, 18, 19, 20], // Koshinetsu
+    5: [21, 22, 23, 24],         // Tokai
+    6: [25, 26, 27, 28, 29, 30],
+    7: [31, 32, 33, 34, 35],
+    8: [36, 37, 38, 39],
+    9: [40, 41, 42, 43, 44, 45, 46],
+    10: [47]
+};
+
+const GALLERY_BASE_URL = 'http://195.15.228.129/japan/';
+
+function renderGalleryPage(page) {
+    var regionParam = urlParam("p");
+    var yearParam = urlParam("year") || '2020';
+    var filteredImages;
+    if (regionParam && areaToPrefectures[regionParam]) {
+        filteredImages = galleryData.filter(function(elem) {
+            var matchRegion = areaToPrefectures[regionParam].includes(Number(elem.region)) || Number(elem.region) === Number(regionParam);
+            var matchYear = !elem.year || elem.year == yearParam;
+            return matchRegion && matchYear;
+        });
+    } else {
+        filteredImages = galleryData.filter(function(elem) {
+            var matchRegion = (elem.region == regionParam || regionParam == null);
+            var matchYear = !elem.year || elem.year == yearParam;
+            return matchRegion && matchYear;
+        });
+    }
+    totalPages = Math.ceil(filteredImages.length / IMAGES_PER_PAGE);
+    var start = (page - 1) * IMAGES_PER_PAGE;
+    var end = start + IMAGES_PER_PAGE;
+    var imagesToShow = filteredImages.slice(start, end);
+    var $gallery = $('.imageGallery1');
+    $gallery.empty();
+    imagesToShow.forEach(function(elem) {
+        var safeTitle = escapeHtml(elem.title);
+        var yearFolder = elem.year || '2020';
+        var imagePath = yearFolder + '/' + elem.url;
+        $gallery.append(
+            "<div class='col-lg-3 col-md-4 col-sm-6 mb-4'>" +
+                "<div class='gallery-card'>" +
+                    "<a href='" + GALLERY_BASE_URL + imagePath + "' class='h_gallery_item' data-sub-html='<h4>" + safeTitle + "</h4>'>" +
+                        "<img src='" + GALLERY_BASE_URL + imagePath + "' alt=''>" +
+                    "</a>" +
+                    "<div class='gallery-desc'>" + safeTitle + "</div>" +
+                "</div>" +
+            "</div>"
+        );
+    });
+    if ($gallery.data('lightGallery')) {
+        $gallery.data('lightGallery').destroy(true);
+    }
+    $gallery.lightGallery({ selector: '.h_gallery_item' });
+    if (imagesToShow.length === 0) {
+        $gallery.html("<p>Il n'y a pas encore de photos pour cette région :(</p>" +
+            "<p>Retourner à la <a href='map.html'>carte</a> ou à la <a href='gallery.html'>galerie</a>.</p>");
+    }
+    renderPagination(page, totalPages);
+}
+
+function renderPagination(page, totalPages) {
+    var $pagination = $('#pagination');
+    $pagination.empty();
+    if (totalPages <= 1) return;
+    var prevDisabled = (page === 1) ? 'disabled' : '';
+    var nextDisabled = (page === totalPages) ? 'disabled' : '';
+    $pagination.append('<button class="btn btn-sm btn-primary" id="prevPage" ' + prevDisabled + '>Précédent</button>');
+    for (var i = 1; i <= totalPages; i++) {
+        var active = (i === page) ? 'active' : '';
+        $pagination.append('<button class="btn btn-sm btn-secondary mx-1 pageBtn ' + active + '" data-page="' + i + '">' + i + '</button>');
+    }
+    $pagination.append('<button class="btn btn-sm btn-primary" id="nextPage" ' + nextDisabled + '>Suivant</button>');
+    $pagination.off('click').on('click', '.pageBtn', function() {
+        var p = parseInt($(this).data('page'));
+        if (p !== page) {
+            currentPage = p;
+            renderGalleryPage(currentPage);
+        }
+    });
+    $pagination.on('click', '#prevPage', function() {
+        if (currentPage > 1) {
+            currentPage--;
+            renderGalleryPage(currentPage);
+        }
+    });
+    $pagination.on('click', '#nextPage', function() {
+        if (currentPage < totalPages) {
+            currentPage++;
+            renderGalleryPage(currentPage);
+        }
+    });
+}
 
 function fillGallery() {
     $.getJSON("img/gallery/image.json", function (data) {
+        galleryData = data.images.slice().reverse();
+        currentPage = 1;
+        renderGalleryPage(currentPage);
         var regionParam = urlParam("p");
         if (regionParam != null) {
-            $('#content').prepend('<h1>Région du ' + definition_of_english_name[regionParam]);
-            $('.isotope_fillter').remove()
-        }
-        $.each(data.images, function (i) {
-            var elem = data.images[data.images.length-1-i];
-            if (elem.region == regionParam || regionParam == null) {
-                $('.imageGallery1').append("<div class='col-lg-3 col-md-4 col-sm-6 " + elem.region + "'>" +
-                    "                <div class='h_gallery_item' data-src='img/gallery/"+elem.url+"'>" +
-                    "                    <img src='img/gallery/" + elem.url + "' alt=''>" +
-                    "                    <div class='hover'>" +
-                    "                        <a><h4>" + elem.title + "</h4></a>" +
-                    "                        <a class='light'><i class='fa fa-expand'></i></a>" +
-                    "                    </div>" +
-                    "                </div>" +
-                    "            </div>")
-            }
-        });
-		
-		$(".imageGallery1").lightGallery({
-			selector: '.h_gallery_item'
-		}); 
-		
-        if ($(".h_gallery_item img").length === 0) {
-            $(".imageGallery1").replaceWith("<p>Il n'y a pas encore de photos pour cette région :(</p>" +
-                "<p>Retourner à la <a href='map.html'>carte</a> ou à la <a href='gallery.html'>galerie</a>.</p>")
+            // $('#content').prepend('<h1>Région du ' + definition_of_english_name[regionParam]);
         }
     });
 }
@@ -47,4 +127,58 @@ var checkExist = setInterval(function () {
         clearInterval(checkExist);
         fillGallery();
     }
-}, 100); // check every 100ms
+}, 100);
+
+$(document).on('click', '.gallery_filter li', function(e) {
+    e.preventDefault();
+    $('.gallery_filter li').removeClass('active');
+    $(this).addClass('active');
+    var filter = $(this).data('filter');
+    if (filter === '*') {
+        history.replaceState(null, '', 'gallery.html');
+        currentPage = 1;
+        renderGalleryPage(currentPage);
+    } else {
+        // Le filtre est du type .3, .6, etc. On enlève le point pour obtenir le numéro de région
+        var region = filter.replace('.', '');
+        history.replaceState(null, '', 'gallery.html?p=' + region);
+        currentPage = 1;
+        renderGalleryPage(currentPage);
+    }
+});
+
+$(document).on('click', '.year_filter li', function(e) {
+    e.preventDefault();
+    var year = $(this).data('year');
+    $('.year_filter li').removeClass('active');
+    $(this).addClass('active');
+    var params = new URLSearchParams(window.location.search);
+    params.set('year', year);
+    window.location.search = params.toString();
+});
+
+function escapeHtml(text) {
+    return text
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/\"/g, "&quot;")
+        .replace(/'/g, "&#39;");
+}
+
+$(function() {
+    var regionParam = urlParam("p");
+    if (regionParam) {
+        $('.gallery_filter li').removeClass('active');
+        var $li = $('.gallery_filter li[data-filter=".' + regionParam + '"]');
+        if ($li.length) {
+            $li.addClass('active');
+        }
+    }
+    var yearParam = urlParam("year") || '2020';
+    $('.year_filter li').removeClass('active');
+    var $yearLi = $('.year_filter li[data-year="' + yearParam + '"]');
+    if ($yearLi.length) {
+        $yearLi.addClass('active');
+    }
+});
--- js/map.js
+++ js/map.js
@@ -2,82 +2,45 @@ $(function () {
     $.getJSON("img/gallery/image.json", function (photos) {
         var areas = [
             {"code": 1, "name": "Hokkaidō", "color": "#ca93ea", "hoverColor": "#e0b1fb", "prefectures": [1]},
-            {
-                "code": 2,
-                "name": "Tōhoku",
-                "color": "#a7a5ea",
-                "hoverColor": "#d6d4fd",
-                "prefectures": [2, 3, 4, 5, 6, 7]
-            },
-            {
-                "code": 3,
-                "name": "Kantō",
-                "color": "#84b0f6",
-                "hoverColor": "#c1d8fd",
-                "prefectures": [8, 9, 10, 11, 12, 13, 14]
-            },
-            {
-                "code": 4,
-                "name": "Koshinetsu",
-                "color": "#52d49c",
-                "hoverColor": "#93ecc5",
-                "prefectures": [15, 16, 17, 18, 19, 20]
-            },
-            {"code": 4, "name": "Tōkai", "color": "#77e18e", "hoverColor": "#aff9bf", "prefectures": [21, 22, 23, 24]},
-            {
-                "code": 6,
-                "name": "Kansai",
-                "color": "#f2db7b",
-                "hoverColor": "#f6e8ac",
-                "prefectures": [25, 26, 27, 28, 29, 30]
-            },
-            {
-                "code": 7,
-                "name": "Chūgoku",
-                "color": "#f9ca6c",
-                "hoverColor": "#ffe5b0",
-                "prefectures": [31, 32, 33, 34, 35]
-            },
-            {
-                "code": 8,
-                "name": "Shikoku",
-                "color": "#fbad8b",
-                "hoverColor": "#ffd7c5",
-                "prefectures": [36, 37, 38, 39]
-            },
-            {
-                "code": 9,
-                "name": "Kyūshū",
-                "color": "#f7a6a6",
-                "hoverColor": "#ffcece",
-                "prefectures": [40, 41, 42, 43, 44, 45, 46]
-            },
+            {"code": 2, "name": "Tōhoku", "color": "#a7a5ea", "hoverColor": "#d6d4fd", "prefectures": [2, 3, 4, 5, 6, 7]},
+            {"code": 3, "name": "Kantō", "color": "#84b0f6", "hoverColor": "#c1d8fd", "prefectures": [8, 9, 10, 11, 12, 13, 14]},
+            {"code": 4, "name": "Koshinetsu", "color": "#52d49c", "hoverColor": "#93ecc5", "prefectures": [15, 16, 17, 18, 19, 20]},
+            {"code": 5, "name": "Tōkai", "color": "#77e18e", "hoverColor": "#aff9bf", "prefectures": [21, 22, 23, 24]},
+            {"code": 6, "name": "Kansai", "color": "#f2db7b", "hoverColor": "#f6e8ac", "prefectures": [25, 26, 27, 28, 29, 30]},
+            {"code": 7, "name": "Chūgoku", "color": "#f9ca6c", "hoverColor": "#ffe5b0", "prefectures": [31, 32, 33, 34, 35]},
+            {"code": 8, "name": "Shikoku", "color": "#fbad8b", "hoverColor": "#ffd7c5", "prefectures": [36, 37, 38, 39]},
+            {"code": 9, "name": "Kyūshū", "color": "#f7a6a6", "hoverColor": "#ffcece", "prefectures": [40, 41, 42, 43, 44, 45, 46]},
             {"code": 10, "name": "Okinawa", "color": "#ea89c4", "hoverColor": "#fdcae9", "prefectures": [47]}
         ];
 
         $("#map").japanMap(
             {
                 areas: areas,
-                selection: "prefecture",
+                selection: "area",
                 borderLineWidth: 0.25,
                 drawsBoxLine: false,
                 showsAreaName: true,
                 width: 800,
                 fontSize: 15,
                 fontColor: "black",
-                onSelect: function (prefecture) {
-                    region = prefecture.area;
-                    var numberPhotos = getNumberPhotosByRegion(region);
+                onSelect: function (region) {
+                    var fullRegion = areas.find(function(a) { return a.code === region.code && a.name === region.name; });
+                    if (!fullRegion) {
+                        console.error('Région non trouvée dans areas:', region);
+                        return;
+                    }
+                    var numberPhotos = getNumberPhotosByRegion(fullRegion);
                     if (numberPhotos === 0) {
-                        $('#exampleModal .modal-body').html("<p>Il n'y a malheureusement pas encore de photos pour cette région :(</p>");
+                        $('#exampleModal .modal-body').html("<p>Il n'y a malheureusement pas encore de photos pour cette région :(");
                         $('#exampleModal .btn-primary').hide()
                     } else {
-                        $('#exampleModal .modal-body').html('<p>Souhaitez-vous accéder aux photos de la région de ' + region.name + ' ? <br>Il y a ' + numberPhotos + ' photo' + (numberPhotos > 1 ? 's' : '') + ' :)</p>');
+                        $('#exampleModal .modal-body').html('<p>Souhaitez-vous accéder aux photos de la région de ' + fullRegion.name + ' ? <br>Il y a ' + numberPhotos + ' photo' + (numberPhotos > 1 ? 's' : '') + ' :)</p>');
                         $('#exampleModal .btn-primary').show()
                     }
                     $('#exampleModal').modal();
-                    $('#exampleModal .btn-primary').on('click', function () {
-                        window.location.href = "gallery.html?p=" + region.code
+                    console.log('region:', fullRegion, 'region.code:', fullRegion.code);
+                    $('#exampleModal .btn-primary').off('click').on('click', function () {
+                        window.location.href = "gallery.html?p=" + fullRegion.code;
                     })
                 }
             }
@@ -87,11 +50,11 @@ $(function () {
             var counter = 0;
             $.each(photos.images, function (i) {
                 var elem = photos.images[i];
-                if (elem.region === region.code) {
+                if (region.prefectures.includes(Number(elem.region)) || Number(elem.region) === Number(region.code)) {
                     counter++;
                 }
             });
-
+            console.log('Région:', region.name, 'Préfectures:', region.prefectures, 'Nombre trouvé:', counter);
             return counter;
         }
     });
