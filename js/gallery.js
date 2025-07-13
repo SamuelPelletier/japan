64e8122b5955a91fe31ae72e025fe55d016efa2d
Samuel Pelletier <pellesam.info@gmail.com>
2025-07-13 19:50:02 +0200

    Fix filter 3

--- js/gallery.js
+++ js/gallery.js
@@ -28,16 +28,14 @@ function renderGalleryPage(page) {
     var yearParam = urlParam("year") || '2020';
     var filteredImages;
     if (regionParam && areaToPrefectures[regionParam]) {
+        // p est un code de grande région
         filteredImages = galleryData.filter(function(elem) {
-            var matchRegion = areaToPrefectures[regionParam].includes(Number(elem.region)) || Number(elem.region) === Number(regionParam);
-            var matchYear = !elem.year || elem.year == yearParam;
-            return matchRegion && matchYear;
+            // Accepte les images dont la région est le code de la grande région OU une préfecture de cette grande région
+            return Number(elem.region) === Number(regionParam) || areaToPrefectures[regionParam].includes(Number(elem.region));
         });
     } else {
         filteredImages = galleryData.filter(function(elem) {
-            var matchRegion = (elem.region == regionParam || regionParam == null);
-            var matchYear = !elem.year || elem.year == yearParam;
-            return matchRegion && matchYear;
+            return (elem.region == regionParam || regionParam == null);
         });
     }
     totalPages = Math.ceil(filteredImages.length / IMAGES_PER_PAGE);
