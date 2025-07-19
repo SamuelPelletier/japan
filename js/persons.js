48f9d0b6e754f6210b12c4425ab852353f44e13e
Samuel Pelletier <pellesam.info@gmail.com>
2025-07-19 19:05:31 +0200

    Change color and add team

---
+++ js/persons.js
@@ -0,0 +1,8 @@
+const PERSONS = [
+    { name: "Samuel", img: "img/team/samuel.jpg" },
+    { name: "Bryan", img: "img/team/bryan.jpg" },
+    { name: "Jonah", img: "img/team/jonah.jpg" },
+    { name: "Basile", img: "img/team/basile.jpg" },
+    { name: "Maria", img: "img/team/maria.jpg" }
+    // Ajoute ici d'autres personnes facilement
+]; 
